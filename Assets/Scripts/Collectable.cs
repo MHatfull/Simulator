0909314870ba754e800ae823c543663aba118f99
MHatfull <max.hatfull@btinternet.com>
2017-12-04 22:00:07 +0000

    Can throw and pick up objects

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Collectable : MonoBehaviour {
+    public Rigidbody Rigidbody;
+    private void Awake()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+    }
+}
