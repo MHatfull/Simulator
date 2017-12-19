e364fefbb26823d16e1e76f5e99d2a5e96f0bc0a
Max Laptop <max.hatfull@btinternet.com>
2017-12-19 23:12:03 +0000

    wip

---
+++ Assets/Scripts/ItemThrower.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpringJoint))]
+public class ItemThrower : MonoBehaviour
+{
+    SpringJoint _spring;
+
+    [SerializeField] List<KeyCode> _throwingKeys;
+    [SerializeField] List<Collectable> _collectables;
+
+    public void Awake()
+    {
+        _spring = GetComponent<SpringJoint>();
+        if(_throwingKeys.Count != _collectables.Count)
+        {
+            throw new System.Exception("You must have the same number of controls as collectables!");
+        }
+    }
+
+    public void ThrowItem()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode key in _throwingKeys)
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    FireObject(_collectables[_throwingKeys.IndexOf(key)]);
+                }
+            }
+        }
+    }
+
+    void FireObject(Collectable prefab)
+    {
+        var newObject = Instantiate(prefab);
+        newObject.transform.position = transform.TransformPoint(_spring.anchor);
+        newObject.Rigidbody.velocity = transform.TransformPoint(Vector3.forward * 10) - transform.position;
+    }
+}
---
+++ Assets/Scripts/PlayerAbilities.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(SpringJoint))]
+[RequireComponent(typeof(ItemThrower))]
+public class PlayerAbilities : NetworkBehaviour
+{
+    SpringJoint _spring;
+    ItemThrower _itemThrower;
+
+    bool _holding;
+    float _scrollBy;
+
+    enum ControlMode { Throwing, Collecting }
+
+    ControlMode _currentMode = ControlMode.Throwing;
+
+    private void Start()
+    {
+        _spring = GetComponent<SpringJoint>();
+        _itemThrower = GetComponent<ItemThrower>();
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+        switch(_currentMode)
+        {
+            case ControlMode.Collecting:
+                {
+                    if (Input.GetKeyDown(KeyCode.Mouse0))
+                    {
+                        if (!_holding)
+                        {
+                            Debug.Log("grab");
+                            Raycast();
+                        }
+                        else
+                        {
+                            Debug.Log("let go");
+                            _holding = false;
+                            _spring.connectedBody = null;
+                        }
+                    }
+                    _scrollBy = Input.GetAxis("Mouse ScrollWheel");
+                    if (_scrollBy != 0)
+                    {
+                        _spring.anchor += new Vector3(0, 0, _scrollBy);
+                    }
+                    break;
+                }
+            case ControlMode.Throwing:
+                {
+                    _itemThrower.ThrowItem();
+                    break;
+                }
+        }
+    }
+
+    void Raycast()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+            if(objectHit.GetComponent<Collectable>() != null)
+            {
+                _holding = true;
+                _spring.connectedBody = objectHit.GetComponent<Rigidbody>();
+            }
+        }
+    }
+
+}
