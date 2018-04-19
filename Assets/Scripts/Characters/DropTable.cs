05b1c7bd09fcefd968799ac63e803b65edf14d61
Max Hatfull <max.hatfull@btinternet.com>
2018-04-19 08:35:41 +0100

    Drop tables

---
+++ Assets/Scripts/Characters/DropTable.cs
@@ -0,0 +1,47 @@
+﻿using Underlunchers.Items;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Underlunchers.Characters
+{
+    [RequireComponent(typeof(Character))]
+    public class DropTable : NetworkBehaviour
+    {
+        Character _self;
+
+        [SerializeField] Drop[] _drops;
+
+        [System.Serializable]
+        struct Drop
+        {
+            [SerializeField] [Range(0, 1)] private float _dropChance;
+            public float DropChance { get { return _dropChance; } }
+            [SerializeField] Collectable _toDrop;
+            public Collectable ToDrop { get { return _toDrop; } }
+        }
+
+        void Start()
+        {
+            if (isServer)
+            {
+                _self = GetComponent<Character>();
+                _self.OnDeath += DropItems;
+            }
+        }
+
+        [Server]
+        private void DropItems()
+        {
+            Debug.Log("dropping loots");
+            foreach(Drop drop in _drops)
+            {
+                if (Random.Range(0f, 1f) < drop.DropChance)
+                {
+                    GameObject toDrop = Instantiate(drop.ToDrop).gameObject;
+                    toDrop.transform.position = transform.position;
+                    NetworkServer.Spawn(toDrop);
+                }
+            }
+        }
+    }
+}
