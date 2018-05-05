b85a30662f46d404616ea719101df1c862a767bd
Max Hatfull <max.hatfull@btinternet.com>
2018-05-05 15:23:32 +0100

    wip

---
+++ Assets/SwitchScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SwitchScene : MonoBehaviour {
+    [SerializeField] string _sceneName;
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(delegate { SceneManager.LoadScene(_sceneName); });
+    }
+}
---
+++ Assets/editable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class editable : MonoBehaviour {
+
+    MeshFilter _meshFilter;
+
+    private void Awake()
+    {
+        _meshFilter = GetComponent<MeshFilter>();
+    }
+}
