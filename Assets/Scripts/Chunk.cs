e57d3a4f279f00a5083ffab52ec81953941f0896
Max Hatfull <max.hatfull@btinternet.com>
2018-05-21 22:06:38 +0100

    Chunked mesh editor

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Underlunchers.Scene
+{
+    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(MeshCollider))]
+    public class Chunk : MonoBehaviour
+    {
+        public delegate void VertexUpdatedHandler(Chunk chunk, Vector2Int vert, float height);
+        public event VertexUpdatedHandler VertexUpdated;
+
+        int _vertsAcross;
+        int _vertsDeep;
+        float _width;
+        float _depth;
+
+        Material _material;
+        MeshCollider _collider;
+
+        MeshFilter _meshFilter;
+        Mesh _mesh;
+
+        List<Vector3> _verts = new List<Vector3>();
+        List<int> _tris = new List<int>();
+        List<Vector2> _uvs = new List<Vector2>();
+
+        float _dist;
+        int? _selectedPoint;
+        Vector3 _old;
+
+        public static Chunk CreateChunk(Vector2 size, Vector2Int vertexSize, Material material)
+        {
+            GameObject newChunkObject = new GameObject();
+            newChunkObject.AddComponent<MeshFilter>();
+            newChunkObject.AddComponent<MeshRenderer>();
+            Chunk newChunk = newChunkObject.AddComponent<Chunk>();
+            newChunk._depth = size.y;
+            newChunk._width = size.x;
+            newChunk._vertsDeep = vertexSize.y;
+            newChunk._vertsAcross = vertexSize.x;
+            newChunk._material = material;
+            return newChunk;
+        }
+
+        private void Start()
+        {
+            _meshFilter = GetComponent<MeshFilter>();
+            _collider = GetComponent<MeshCollider>();
+
+            _mesh = GenerateStartMesh();
+            _meshFilter.mesh = _mesh;
+            GetComponent<MeshRenderer>().material = _material;
+
+            _collider.sharedMesh = _mesh;
+        }
+
+        Mesh GenerateStartMesh()
+        {
+            List<Vector3> verts = new List<Vector3>();
+            List<Vector2> uvs = new List<Vector2>();
+            for (int i = 0; i < _vertsAcross; i++)
+            {
+                for (int j = 0; j < _vertsDeep; j++)
+                {
+                    Vector3 vert = new Vector3(((float)i / (_vertsAcross - 1)) * _width, 0, ((float)j / (_vertsDeep - 1)) * _depth);
+                    verts.Add(vert);
+                    uvs.Add(new Vector2((float)i / (_vertsAcross - 1), (float)j / (_vertsDeep - 1)));
+                }
+            }
+            _verts = verts;
+            _tris = CalculateTriangles();
+            _uvs = uvs;
+            Mesh mesh = new Mesh { vertices = _verts.ToArray(), triangles = _tris.ToArray(), uv = _uvs.ToArray() };
+            mesh.RecalculateNormals();
+            return mesh;
+        }
+
+        private List<int> CalculateTriangles()
+        {
+            List<int> tris = new List<int>();
+            for (int i = 0; i < _vertsAcross - 1; i++)
+            {
+                for (int j = 0; j < _vertsDeep - 1; j++)
+                {
+                    tris.Add(GetIndex(i, j));
+                    tris.Add(GetIndex(i, j + 1));
+                    tris.Add(GetIndex(i + 1, j + 1));
+
+                    tris.Add(GetIndex(i, j));
+                    tris.Add(GetIndex(i + 1, j + 1));
+                    tris.Add(GetIndex(i + 1, j));
+                }
+            }
+
+            return tris;
+        }
+
+        private void OnMouseDown()
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(UnityEngine.Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                List<Vector3> triangle = new List<Vector3>
+                    {
+                        _mesh.vertices[_mesh.triangles[hit.triangleIndex * 3]],
+                        _mesh.vertices[_mesh.triangles[hit.triangleIndex * 3 + 1]],
+                        _mesh.vertices[_mesh.triangles[hit.triangleIndex * 3 + 2]]
+                    };
+                Vector3 point = triangle.OrderBy((a) => Vector3.Distance(a, hit.transform.InverseTransformPoint(hit.point))).ToList()[0];
+                _selectedPoint = _verts.IndexOf(point);
+                _old = point;
+                _dist = Vector3.Distance(transform.TransformPoint(point), Camera.main.transform.position);
+            }
+        }
+
+        private void OnMouseUp()
+        {
+            if(VertexUpdated != null)
+            {
+                int x = _selectedPoint.Value / _vertsDeep;
+                int y = _selectedPoint.Value % _vertsDeep;
+                Debug.Log("changed " + x + ", " + y);
+                VertexUpdated(this, new Vector2Int(x, y), _verts[_selectedPoint.Value].y);
+            }
+            _selectedPoint = null;
+        }
+
+        private void Update()
+        {
+            if (_selectedPoint.HasValue)
+            {
+                Vector3 raw = transform.InverseTransformPoint((Camera.main.transform.position + (Camera.main.ScreenPointToRay(UnityEngine.Input.mousePosition).direction.normalized * _dist)));
+                UpdateVertex(_selectedPoint.Value, raw.y);
+            }
+        }
+
+        public void UpdateVertex(Vector2Int vert, float val)
+        {
+            Debug.Log("updating " + vert.x + ", " + vert.y);
+            UpdateVertex(GetIndex(vert.x, vert.y), val);
+        }
+
+        private void UpdateVertex(int vert, float val)
+        {
+            Vector2 flat = new Vector2(_verts[vert].x, _verts[vert].z);
+            _verts[vert] = new Vector3(flat.x, val, flat.y);
+            _mesh.vertices = _verts.ToArray();
+            _mesh.RecalculateNormals();
+        }
+
+        private int GetIndex(int x, int y)
+        {
+            return (x * _vertsDeep) + y;
+        }
+    }
+}
---
+++ Assets/Scripts/EditableMesh.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Underlunchers.Scene
+{
+    public class EditableMesh : MonoBehaviour
+    {
+        [SerializeField] Vector2Int _chunkVerts;
+        [SerializeField] Vector2 _chunkSize;
+        [SerializeField] Vector2Int _numChunks;
+        [SerializeField] Material _material;
+
+        Chunk[][] _chunks;
+        Dictionary<Vector2Int, List<Chunk>> _chunksAtVerts = new Dictionary<Vector2Int, List<Chunk>>();
+        Dictionary<Chunk, Vector2Int> _chunkLocations = new Dictionary<Chunk, Vector2Int>();
+        float[][] _heightMap;
+
+        private void Awake()
+        {
+            _chunks = new Chunk[_numChunks.x][];
+            _heightMap = new float[_numChunks.x * (_chunkVerts.x - 1) + 1][];
+            CreateStartChunks();
+        }
+
+        private void CreateStartChunks()
+        {
+            for (int i = 0; i < _numChunks.x; i++)
+            {
+                _chunks[i] = new Chunk[_numChunks.y];
+                for (int j = 0; j < _numChunks.y; j++)
+                {
+                    Chunk chunk = CreateChunk(i, j);
+                    Vector2Int chunkLoc = new Vector2Int(i * (_chunkVerts.x - 1), j * (_chunkVerts.y - 1));
+                    _chunkLocations.Add(chunk, chunkLoc);
+                    PrefillHeightmap(i, j, chunk);
+                }
+            }
+        }
+
+        private void PrefillHeightmap(int i, int j, Chunk c)
+        { 
+            for (int x = 0; x < _chunkVerts.x; x++)
+            {
+                _heightMap[i * (_chunkVerts.x - 1) + x] = new float[_numChunks.y * (_chunkVerts.y - 1) + 1];
+                for (int y = 0; y < _chunkVerts.y; y++)
+                {
+                    Vector2Int v = new Vector2Int(i * (_chunkVerts.x - 1) + x, j * (_chunkVerts.y - 1) + y);
+                    if (!_chunksAtVerts.ContainsKey(v))
+                    {
+                        _chunksAtVerts.Add(v, new List<Chunk> { c });
+                    }
+                    else
+                    {
+                        _chunksAtVerts[v].Add(c);
+                    }
+                    _heightMap[v.x][v.y] = 0;
+                }
+            }
+        }
+
+        private Chunk CreateChunk(int i, int j)
+        {
+            Chunk chunk = Chunk.CreateChunk(_chunkSize, _chunkVerts, _material);
+            chunk.transform.parent = transform;
+            chunk.transform.position = new Vector3(i * _chunkSize.x, 0, j * _chunkSize.y);
+            chunk.VertexUpdated += Smooth;
+            _chunks[i][j] = chunk;
+            return chunk;
+        }
+
+        private void Smooth(Chunk chunk, Vector2Int vert, float height)
+        {
+            Vector2Int l = _chunkLocations[chunk];
+            _heightMap[l.x + vert.x][l.y + vert.y] = height;
+            List<Vector2Int> toUpdate = new List<Vector2Int> { new Vector2Int(l.x + vert.x, l.y + vert.y) };
+
+            UpdateChunks(toUpdate);
+        }
+
+        void UpdateChunks(List<Vector2Int> updated)
+        {
+            foreach(Vector2Int pos in updated)
+            {
+                UpdateChunks(pos);
+            }
+        }
+
+        void UpdateChunks(Vector2Int pos)
+        {
+            float height = _heightMap[pos.x][pos.y];
+            Debug.Log("pos is " + pos.x + ", " + pos.y);
+            foreach(Chunk c in _chunksAtVerts[pos])
+            {
+                Vector2Int chunkLocation = _chunkLocations[c];
+                c.UpdateVertex(pos - chunkLocation, height);
+            }
+        }
+    }
+}
